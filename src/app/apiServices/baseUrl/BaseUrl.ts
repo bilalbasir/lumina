61da162ae3fd7664bbc08477a9255ee9c78d72af
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-09-24 18:28:29 +0500

    UPDATE:remove logs from  base url file

--- src/app/apiServices/baseUrl/BaseUrl.ts
+++ src/app/apiServices/baseUrl/BaseUrl.ts
@@ -1,11 +1,8 @@
-// export const baseUrl = "https://lumina-talent-advisory-backend.onrender.com/api"
-// export const imageBaseUrl = "https://lumina-talent-advisory-backend.onrender.com/uploads"
 
-// export const cloudinaryBaseUrl = "https://res.cloudinary.com/df0tssvk4/image/upload"
 export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 export const imageBaseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL
 
 export const cloudinaryBaseUrl = process.env.NEXT_PUBLIC_CLOUDINARY_URL
 
-console.log("LOG BASE URL", process.env.NEXT_PUBLIC_BASE_URL);
+
 
