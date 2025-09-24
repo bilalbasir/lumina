0226c5a43b9ceaedc1cc1465552e8eb2510d99a5
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-09-24 18:20:03 +0500

    UPDATE:update base url

--- src/app/apiServices/baseUrl/BaseUrl.ts
+++ src/app/apiServices/baseUrl/BaseUrl.ts
@@ -7,4 +7,5 @@ export const imageBaseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL
 
 export const cloudinaryBaseUrl = process.env.NEXT_PUBLIC_CLOUDINARY_URL
 
+console.log("LOG BASE URL", process.env.NEXT_PUBLIC_BASE_URL);
 
