3ba8dc1c3c4fdafd945226401bca987c200255fb
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-09-24 18:41:28 +0500

    UPDATE:remove logs from  base url file

--- src/app/apiServices/baseUrl/BaseUrl.ts
+++ src/app/apiServices/baseUrl/BaseUrl.ts
@@ -4,7 +4,6 @@ export const imageBaseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL!
 
 export const cloudinaryBaseUrl = process.env.NEXT_PUBLIC_CLOUDINARY_URL!
 
-console.log("BASE URL", baseUrl, "process.env.NEXT_PUBLIC_BASE_URL!", process.env.NEXT_PUBLIC_BASE_URL!);
 
 
 
